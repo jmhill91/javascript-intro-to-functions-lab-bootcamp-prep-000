8be5aab3c8460222d18588f73357aaa45c8abd7c
John Hill <john.m.hill91@gmail.com>
2019-01-09 04:49:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function logShout(string) {
 function logWhisper(string){
   console.log('hello')
 }
+function sayHiToGrandma(string)
 function sayHiToGrandma(string){
   if(string===string.toLowerCAse()){
  return "I can\'t hear you!"
